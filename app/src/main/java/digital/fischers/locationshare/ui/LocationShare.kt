7609abde3dce85465faa1b06cbc79a7cca509be0
Importantus <importantus@mailbox.org>
2024-12-29 16:43:05 +0100

    :recycle: Refactor to use the new single onboarding screen

--- app/src/main/java/digital/fischers/locationshare/ui/LocationShare.kt
+++ app/src/main/java/digital/fischers/locationshare/ui/LocationShare.kt
@@ -1,6 +1,5 @@
 package digital.fischers.locationshare.ui
 
-import android.Manifest
 import android.content.Intent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import digital.fischers.locationshare.ui.onboarding.OnboardingWrapper
-import digital.fischers.locationshare.ui.onboarding.permissions.PermissionsScreen
-import digital.fischers.locationshare.ui.onboarding.server.SetServerScreen
-import digital.fischers.locationshare.ui.onboarding.welcome.WelcomeScreen
+import digital.fischers.locationshare.ui.onboarding.OnboardingScreen
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -36,45 +29,10 @@ fun LocationShareApp(
             composable(Screen.Home.route) {
                 Map()
             }
-            navigation(Screen.Welcome.route, Screen.Onboarding.route) {
-                val numberOfSteps = 4
-
-                composable(Screen.Welcome.route) {
-                    WelcomeScreen(onNavigateNext = {
-                        appState.navigateToPermissions()
-                    })
-                }
-                composable(Screen.Permissions.route) {
-                    val rememberBackgroundPermission = rememberPermissionState(
-                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                    )
-
-                    OnboardingWrapper(
-                        step = 1,
-                        steps = numberOfSteps,
-                        onNavigateNext = if (rememberBackgroundPermission.status.isGranted) {
-                            { appState.navigateToSetServer() }
-                        } else null
-                    ) {
-                        PermissionsScreen(
-                            onBackNavigation = { appState.navigateToWelcome() },
-                            onNextNavigation = { appState.navigateToSetServer() }
-                        )
-                    }
-                }
-                composable(Screen.SetServer.route) {
-                    OnboardingWrapper(
-                        step = 2,
-                        steps = numberOfSteps,
-                        onNavigateNext = {
-                        }
-                    ) {
-                        SetServerScreen(
-                            onBackNavigation = { appState.navigateToPermissions() },
-                            onNextNavigation = {}
-                        )
-                    }
-                }
+            composable(Screen.Onboarding.route) {
+                OnboardingScreen(onNavigateNext = {
+                    appState.navigateHome()
+                })
             }
         }
     }
