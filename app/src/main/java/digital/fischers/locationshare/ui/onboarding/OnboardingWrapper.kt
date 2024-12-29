9ca3be9c9cd4aa1d46336148d4e6dca99221c583
Importantus <importantus@mailbox.org>
2024-12-29 16:43:31 +0100

    :dizzy: Add animations and transitions to onboarding

--- app/src/main/java/digital/fischers/locationshare/ui/onboarding/OnboardingWrapper.kt
+++ app/src/main/java/digital/fischers/locationshare/ui/onboarding/OnboardingWrapper.kt
@@ -1,5 +1,7 @@
 package digital.fischers.locationshare.ui.onboarding
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,17 +9,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import digital.fischers.locationshare.R
 import digital.fischers.locationshare.ui.components.buttons.ButtonRound
@@ -79,27 +84,50 @@ fun OnboardingWrapper(
                 modifier = Modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ) {
-                Row(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(20.dp))
-                        .background(MaterialTheme.colorScheme.surfaceContainerLowest)
-                        .padding(horizontal = 10.dp, vertical = 6.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    repeat(steps) {
-                        val color =
-                            if (it == step) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.surfaceContainerHigh
-                        Box(
-                            modifier = Modifier
-                                .clip(MaterialTheme.shapes.small)
-                                .background(color)
-                                .padding(5.dp)
-                        )
-                    }
-                }
+                DotIndicator(steps, step)
             }
         }
         screen()
+    }
+}
+
+@Composable
+fun DotIndicator(steps: Int, step: Int) {
+    val offsetX by animateDpAsState(
+        targetValue = (step * 8).dp + ((step) * 10).dp,
+        animationSpec = tween(durationMillis = 300), label = ""
+    )
+
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(20.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerLowest)
+            .padding(horizontal = 10.dp, vertical = 6.dp),
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            repeat(steps) {
+                Box(
+                    modifier = Modifier
+                        .clip(MaterialTheme.shapes.small)
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .padding(5.dp)
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .offset {
+                    IntOffset(
+                        offsetX.roundToPx(),
+                        0
+                    )
+                }
+                .clip(MaterialTheme.shapes.small)
+                .background(MaterialTheme.colorScheme.onSurface)
+                .padding(5.dp)
+        )
     }
 }
